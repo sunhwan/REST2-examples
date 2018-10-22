65a9e0e7abe06d83738578b1b30675466d25b4a9
Sunhwan Jo <sunhwanj@gmail.com>
2018-10-22 07:41:14 +0200

    add fep example

--- NAMD/aaqaa3/run.sh
+++ NAMD/aaqaa3/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MAX_ICNT=50
+NAMD_DIR=/home/sunhwan/local/namd/2.13b1
 
 let pcnt=$icnt-1
 let pstep=1000*$icnt
@@ -11,11 +12,11 @@ sed -e "s/\$pcnt/$pcnt/g" -e "s/\$pstep/$pstep/g" -e "s/\$istep/$istep/g" base.c
 
 if [ $icnt -lt $MAX_ICNT ]
 then
-  qsub --dependencies $COBALT_JOBID -t 1:00:00 -n 128 -A $PROJECT --env icnt=$ncnt:n=$n:nrep=$nrep --mode script run.sh
+  qsub --dependencies $COBALT_JOBID -t 1:00:00 -n 128 -A $PROJECT --env icnt=$ncnt:n=$n:nrep=$nrep run.sh
 fi 
 ./make_output_dirs.sh output $nrep
 let np=$n*16*$nrep
 let ppn=4
 
-runjob -n $np -p 16 --block $COBALT_PARTNAME --verbose=INFO : /home/sunhwanj/US-REST2/NAMD_Develop_Source/BlueGeneQ-xlC/namd2  +replicas $nrep job$icnt.conf +stdout output/%d/job$icnt.%d.log +ppn $ppn
+$NAMD_DIR/namd2 +replicas $nrep job$icnt.conf +stdout output/%d/job$icnt.%d.log +ppn $ppn
 
