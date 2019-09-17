163950f36d495a032cacecc36ad93816766363f6
Sunhwan Jo <sunhwan@silcsbio.com>
2019-09-17 17:01:44 +0900

    update run.sh

--- NAMD/aaqaa3/3_rest2/run.sh
+++ NAMD/aaqaa3/3_rest2/run.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-MAX_ICNT=50
-NAMD_DIR=/home/sunhwan/local/namd/2.13b1-netlrts
+#let icnt=0         # current run number
 
-let nrep=16
-let icnt=0
+NREPLICA=16        # number of replica
+REPLICA_NODE=1     # number of node per replica
+REPLICA_PPN=1      # number of processor per replica
+OUTPUDIR=output    # output directory name
+STEPS_PER_RUN=1000 # steps_per_run in rest2.conf
+MAX_ICNT=50        # maximum number of runs
+NAMD_DIR=/opt/namd # directory contains namd2 and charmrun
 
+# DO NOT EDIT BELOW
 let pcnt=$icnt-1
-let pstep=1000*$icnt
-let istep=$pstep+1000
+let pstep=$STEPS_PER_RUN*$icnt
+let istep=$pstep+$STEPS_PER_RUN
 let ncnt=$icnt+1
 
 sed -e "s/\$pcnt/$pcnt/g" -e "s/\$pstep/$pstep/g" -e "s/\$istep/$istep/g" base.conf > job$icnt.conf
 
 if [ $icnt -lt $MAX_ICNT ]
 then
+  # submit and hold for the next job
   echo
   #qsub --dependencies $COBALT_JOBID -t 1:00:00 -n 128 -A $PROJECT --env icnt=$ncnt:n=$n:nrep=$nrep run.sh
 fi 
 
-./make_output_dirs.sh output $nrep
-let nodes=1
-let ppn=1
-let np=$nodes*$nrep
+./make_output_dirs.sh $OUTPUTDIR $NREPLICA
+let np=$REPLICA_NODE*$REPLICA_PPN
 
-$NAMD_DIR/charmrun +p$np $NAMD_DIR/namd2 +replicas $nrep job$icnt.conf +stdout output/%d/job$icnt.%d.log
+$NAMD_DIR/charmrun +p$np $NAMD_DIR/namd2 +replicas $nrep job$icnt.conf +stdout $OUTPUTDIR/%d/job$icnt.%d.log
 
